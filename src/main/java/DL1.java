b4a832144f284a7273dab6b73a2b81434c3a35cd
Markant <mark.perov@stud.fh-campuswien.ac.at>
2022-09-25 13:25:48 +0200

    DL1 first Question

---
+++ src/main/java/DL1.java
@@ -0,0 +1,43 @@
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+class Dl1 {
+
+    static NumberFormat formatter = new DecimalFormat("###.##################");
+
+    public static String transmissionDelayCalculator( Integer transmissionRate, Integer packetLengthInBits ){
+
+        double transmissionDelay;
+
+        transmissionDelay = packetLengthInBits/(transmissionRate * Math.pow(10, 6));
+
+        double transmissionDelayInMilliseconds = transmissionDelay * 10000;
+
+        String transmissionDelayInDecimalToString = formatter.format(transmissionDelayInMilliseconds);
+
+        return transmissionDelayInDecimalToString + " Milliseconds";
+    }
+
+    public static String propagationDelayCalculator(Integer distance, Double propagationSpeed){
+
+        double propagationDelay;
+
+        propagationDelay = (((distance * Math.pow(10, 3))) / (propagationSpeed * Math.pow(10, 8)));
+
+        double propagationDelayInMilliseconds = propagationDelay * 1000;
+
+        String transmissionDelayInDecimalToString = formatter.format(propagationDelayInMilliseconds);
+
+        return transmissionDelayInDecimalToString + " Milliseconds";
+    }
+
+    public static void main(String[] args) {
+
+        String transmissionDelay = Dl1.transmissionDelayCalculator(1000, 8000); //
+        String propagationDelay = Dl1.propagationDelayCalculator(1,1.1);
+
+        System.out.println( "Transmission Delay is: " + transmissionDelay);
+        System.out.println( "Propagation Delay is: " + propagationDelay);
+
+    }
+}
